096005794e33b7243e57e9edb2178dbc86ed2f17
Anthony Delaby <pro@anthonydelaby.me>
2017-03-22 10:51:02 +0100

    Set custom `OUTPUT_PATH`

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
+OUTPUT_PATH = '../adelaby.github.io/'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
