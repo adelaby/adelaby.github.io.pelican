2e9aee16d2a434462a64be7fbe20c3ad54b30a67
Anthony Delaby <pro@anthonydelaby.me>
2017-03-28 16:32:29 +0200

    Bypass caching system

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,6 +32,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = False
 
 OUTPUT_PATH = '../adelaby.github.io/'
+LOAD_CONTENT_CACHE = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
